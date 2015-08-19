02f836a3b77ad076222709ad32165d94027486b9
Patti Chan <patti@pattichan.com>
2015-08-18 17:55:46 -0700

    Remove dependency on hardcoding ResourseTypeID (use a more friendly ResourceTypeName match instead); only return resources that are set to visible

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,6 +4,10 @@ class Space < NexudusBase
     self.class.get("/spaces/resources/#{id}")
   end
 
+  def get_category(type_name = "Cooking Blocks")
+    # Get the ResourceTypeID of the kind of resource we want, so we can filter by it 
+  end
+
   def resource_location(id)
     # We can only access the "Description", AND the "LinkedResources" data when calling a single Resource
     resource = get_resource(id)
@@ -12,13 +16,12 @@ class Space < NexudusBase
     return linked["Description"]
   end
 
-  def resources
-    type = "Cooking Blocks"
-    type = 41037021
-    results = self.class.get("/spaces/resources?Resource_ResourceType=#{type}")["Records"]
+  def resources(type = "Cooking Blocks", visible = true)
+    results = self.class.get("/spaces/resources?Resource_Visible=#{visible}")["Records"]
     resources = []
 
     results.each do |r|
+      next unless r["ResourceTypeName"] == type
       item = {:id => r["Id"], :name => r["Name"], :location => resource_location(r["Id"])}
       resources << item
     end
