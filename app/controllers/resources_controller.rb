643d29f9233a78a35320d3d9faa95d2b212261c8
Patti Chan <patti@pattichan.com>
2015-09-02 16:55:16 -0700

    Further filter in controller/view by both offered-availability, and not booked.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,14 @@ class ResourcesController < ApplicationController
 
   def index
     params["available"] ||= false
-    @resources = params["available"] ? Space.new.available_resources_by_day_and_time : Space.new.resources
+    space = Space.new
+
+    if params["available"]  
+      offered_resource_ids = space.available_resources_by_day_and_time()
+      @resources = space.booked_resources_by_datetime(offered_resource_ids)
+    else
+      @resources = space.resources
+    end
 
     respond_to do |format|
       format.js { render :json => @resources }
