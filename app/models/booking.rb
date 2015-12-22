8bdb7842df220bac2c82c3cbe75e7b834fcd4516
Patti Chan <patti@pattichan.com>
2015-12-21 19:00:59 -0800

    Just switch to cleaner syntax.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,28 +2,23 @@ class Booking < NexudusBase
 
   @@request_uri = "/spaces/bookings?"
 
-  class << self
+  def self.all(coworkerID="", resource_ids=[], include_passed=false)
+    request_params = []
+    request_params += ["Booking_Coworker=#{coworker_id}"] if coworkerID.present?
+    request_params += ["Booking_Invoiced=false"] unless include_passed # Relies on 'Booking_Invoiced' to guess at whether it's passed or future; maybe there is/will be a better way from Nexudus API
 
-    def all(coworkerID="", resource_ids=[], include_passed=false)
-      request_params = []
-      request_params += ["Booking_Coworker=#{coworker_id}"] if coworkerID.present?
-      request_params += ["Booking_Invoiced=false"] unless include_passed # Relies on 'Booking_Invoiced' to guess at whether it's passed or future; maybe there is/will be a better way from Nexudus API
-
-      bookings = []
-      if resource_ids.present?
-        resource_ids.uniq.each do |id|
-          bookings << get(@@request_uri+(request_params + ["Booking_Resource=#{id}"]).join("&"))["Records"]
-        end
-      else
-        bookings << get(@@request_uri+request_params.join("&"))["Records"]
+    bookings = []
+    if resource_ids.present?
+      resource_ids.uniq.each do |id|
+        bookings << get(@@request_uri+(request_params + ["Booking_Resource=#{id}"]).join("&"))["Records"]
       end
-      bookings = bookings.flatten.reject &:blank?
-      bookings = bookings.sort_by{|b| b["FromTime"]}
-      return bookings.reverse
+    else
+      bookings << get(@@request_uri+request_params.join("&"))["Records"]
     end
-
+    bookings = bookings.flatten.reject &:blank?
+    bookings = bookings.sort_by{|b| b["FromTime"]}
+    return bookings.reverse
   end
-
 
   def create(json_hash)
     self.class.post(@@request_uri, :body => json_hash, :headers => { 'Content-Type' => 'application/json' })
