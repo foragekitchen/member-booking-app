e4982b5cfa1a054abc863a8aaa36fa582a754e9d
andy <mailto.alekseenko@gmail.com>
2016-06-06 13:19:30 +0300

    Added empty templates for error pages; changed layout a little bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+  ERROR_ACTIONS = [:not_found, :changes_rejected, :internal_server_error]
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :check_session
+  before_action :check_session, except: ERROR_ACTIONS
+  before_action :set_coworker, only: ERROR_ACTIONS
 
   def check_session
     return redirect_to_login('Please log in.') unless session[:user_id]
@@ -18,7 +21,23 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def not_found
+    render(status: 404)
+  end
+
+  def changes_rejected
+    render(status: 422)
+  end
+
+  def internal_server_error
+    render(status: 500)
+  end
+
   private
+
+  def set_coworker
+    @coworker = Coworker.find_by_user(session[:user_id]) if session[:user_id].present?
+  end
 
   def redirect_to_login(message = nil)
     reset_session
