9405f2fb4d1191c0b402233bd20c2e989e7a2bfa
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2016-07-25 17:03:44 +0000

    Fix add calendar time zone and link text

--- config/initializers/datetime_formats.rb
+++ config/initializers/datetime_formats.rb
@@ -3,7 +3,7 @@
  booking_day: '%m/%d/%Y',
  booking_time: '%l:%M %p',
  nexudus: '%Y-%m-%dT%H:%M:00Z',
- google_calendar: '%Y%m%dT%H%M00',
+ google_calendar: '%Y%m%dT%H%M00Z',
  universal_date: '%m/%d/%YT%l:%M %p %z'}.each do |name, format|
   Time::DATE_FORMATS[name] = format
 end
