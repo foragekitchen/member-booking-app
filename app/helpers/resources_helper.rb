d58d12f7f383d60e8991673ddaa8eb20c835c0c3
andy <mailto.alekseenko@gmail.com>
2016-09-14 07:27:43 +0000

    Added offset for kitchen map

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -3,7 +3,7 @@ module ResourcesHelper
     return { top: 0, left: 0 } unless resource.location
     scale = 0.76 # Used for converting from inches (distance away from top left corner of space) to pixels (representing on the image)
     offset_top = 55 # Offset for extra border on the map around the actual space
-    offset_left = 45
+    offset_left = 45 - 260
     { top: resource.location.first * scale + offset_top, left: resource.location.last * scale + offset_left }
   end
 end
