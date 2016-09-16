ee51d14bef202dae3a8bb9e909a65abbf323aa17
andy <mailto.alekseenko@gmail.com>
2016-09-16 08:53:41 +0000

    Changed popovers styles; changed recurring booking error message

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -49,7 +49,7 @@ class BookingsController < ApplicationController
       if result
         flash[:notice] = "Your #{'booking'.pluralize(dates.count)} #{'was'.pluralize(dates.count)} successfully saved!"
       else
-        flash[:alert] = 'Some of your bookings may not be saved because of an error'
+        flash[:alert] = "Some of your bookings may not be saved because one of the #{current_user.maker? ? 'tables' : 'stations'} has already been booked"
       end
     else
       date_times = process_date_times(params['bookingDate'], params['bookingFrom'], params['bookingTo'])
