e434c39a60478055aee3ec904969576685fb4dd5
Ivan Ivashchenko <sinerror149@gmail.com>
2020-06-26 16:22:31 +0300

    Use HTTPS for base nexudus uri

--- app/models/nexudus_base.rb
+++ app/models/nexudus_base.rb
@@ -1,6 +1,6 @@
 class NexudusBase
   include HTTParty
-  base_uri 'spaces.nexudus.com/api'
+  base_uri 'https://spaces.nexudus.com/api'
   basic_auth Rails.application.secrets.nexudus_username, Rails.application.secrets.nexudus_password
   # debug_output $stdout # <= will spit out all request details to the console
 
